825994b92a691a9f3e8bfc85a9e3708bca42fcea
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2019-10-27 14:13:39 +1100

    Add files via upload

--- mcm.bat
+++ mcm.bat
@@ -3,7 +3,7 @@
 @echo .............................
 for %%f in (./src/*.mp4) do echo file %%f >> ./src/list.txt; done 
 cd src 
-ffmpeg -f concat -safe 0 -i list.txt -vf scale=1280:-1 stitched-video.mp4
+ffmpeg -loop 1 -i Background.png -f concat -safe 0 -i list.txt -filter_complex "[1:v]scale=720:-1[fg];[0:v][fg]overlay=(W-w)/2:(H-h)/2:shortest=1" output.mp4
 del list.txt
 @echo Meme Compilation Compiled!
 @pause
